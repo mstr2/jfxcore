e1c576f9e3f5ce99da23a5a6430bdc8289f7d3a8
mstr2 <43553916+mstr2@users.noreply.github.com>
2022-06-04 15:48:18 +0200

    doc changes

--- modules/javafx.graphics/src/main/java/javafx/scene/command/StandardCommandCapabilities.java
+++ modules/javafx.graphics/src/main/java/javafx/scene/command/StandardCommandCapabilities.java
@@ -1,13 +1,44 @@
+/*
+ * Copyright (c) 2022, JFXcore. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  JFXcore designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 package javafx.scene.command;
 
 import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.util.Incubating;
 
+/**
+ * Contains a set of standard capabilities that can be reported by commands.
+ * <p>
+ * Using standard capabilities allows {@link CommandHandler} implementations to depend on
+ * standard interfaces, rather than concrete command implementations.
+ *
+ * @since JFXcore 18
+ */
+@Incubating
 public final class StandardCommandCapabilities {
 
     private StandardCommandCapabilities() {}
 
     /**
-     * Marker interface for a command that implements the <em>message</em> capability.
+     * Marks a command that implements the <em>message</em> capability.
      */
     public interface Message {
         /**
@@ -26,7 +57,7 @@ public final class StandardCommandCapabilities {
     }
 
     /**
-     * Marker interface for a command that implements the <em>title</em> capability.
+     * Marks a command that implements the <em>title</em> capability.
      */
     public interface Title {
         /**
